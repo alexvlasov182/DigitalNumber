57238cdeca3d04e75b8bbc3048598fc9d7688174
Alex Vlasov <drumlife182@gmail.com>
2023-03-30 15:46:11 +0200

    Created base structure for task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
 ﻿namespace System;
+
+class DigitalNumbers
+{
+  static void Main()
+  {
+    // validation
+
+    try
+    {
+      // print output
+    }
+    catch (System.Exception)
+    {
+
+      throw;
+    }
+  }
+
+  // create class for validation
+
+  // create data for our task
+}
